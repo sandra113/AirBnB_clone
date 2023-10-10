bf91c88c1c2094de61c4c5c49cb955892e85a731
root <sandramabonga303@gmail.com>
2023-10-10 12:11:35 -0700

    test_case

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,40 @@
+from models.base_model import BaseModel
+import unittest
+import uuid
+from datetime import datetime
+""" Test case for BaseModel """
+
+
+class TestBaseModel(unittest.TestCase):
+    """ Test case for BaseModel """
+
+
+    def test_init(self):
+        """ Test case for class BaseModel __init__ method"""
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel, msg=None)
+        self.assertIsInstance(obj.id, str, msg=None)
+        self.assertIsInstance(obj.created_at, datetime, msg=None)
+        self.assertIsInstance(obj.updated_at, datetime, msg=None)
+
+    def test_save(self):
+        """ Test case for BaseModel save method """
+        obj = BaseModel()
+        original_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(original_update_at, obj.updated_at, msg=None)
+    def test_to_dict(self):
+        """ Test case for BaseModel to_dict method """
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict, msg=None)
+        self.assertIn('__class__', obj_dict, msg=None)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel', msg=None)
+        self.assertEqual(obj_dict['id'], obj.id, msg=None)
+        self.assertEqual(obj_dict['created_at'], 
+                obj.created_at.isoformat(), msg=None)
+        self.assertEqual(obj_dict['updated_at'],
+                obj.updated_at.isoformat(), msg=None)
+
+if __name__ == '__main__':
+    unittest.main()
