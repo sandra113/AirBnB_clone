e55af2b4bb3a18fd6d5f850c8a039511fdab2598
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-10-12 03:11:27 -0700

    pycodestyle errors correction, test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 import sys
 import os
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(cur_dir, '../..'))
-from models.base_model import BaseModel
 import unittest
 import uuid
 from datetime import datetime
+from models.base_model import BaseModel
+
 """ Test case for BaseModel """
 
 
 class TestBaseModel(unittest.TestCase):
     """ Test case for BaseModel """
-
 
     def test_init(self):
         """ Test case for class BaseModel __init__ method"""
@@ -32,6 +30,7 @@ class TestBaseModel(unittest.TestCase):
         original_updated_at = obj.updated_at
         obj.save()
         self.assertNotEqual(original_updated_at, obj.updated_at, msg=None)
+
     def test_to_dict(self):
         """ Test case for BaseModel to_dict method """
         obj = BaseModel()
@@ -39,11 +38,9 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(obj_dict, dict, msg=None)
         self.assertIn('__class__', obj_dict, msg=None)
         self.assertEqual(obj_dict['__class__'], 'BaseModel', msg=None)
-        self.assertEqual(obj_dict['id'], obj.id, msg=None)
-        self.assertEqual(obj_dict['created_at'], 
-                obj.created_at.isoformat(), msg=None)
-        self.assertEqual(obj_dict['updated_at'],
-                obj.updated_at.isoformat(), msg=None)
+        self.assertEqual(obj_dict['id'], obj.id)
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
 
     def test_str(self):
         """Test the __str__ method of BaseModel."""
@@ -51,6 +48,7 @@ class TestBaseModel(unittest.TestCase):
         my_str = str(obj)
         my_test = f"[{obj.__class__.__name__}] ({obj.id}) {obj.__dict__}"
         self.assertEqual(my_str, my_test)
+
 
 if __name__ == '__main__':
     unittest.main()
