2803dd9e49814e195fb0e80efad6d54ee0475d6b
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-10-12 01:42:11 -0700

    kwargs on __init__

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ from datetime import datetime
 class BaseModel:
     """ This class defines all attributes/methods of other classes """
 
-    def __init__(self):
+    def __init__(self, *arg, **kwargs):
         """ Object instantiation """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns a string representation of the class"""
